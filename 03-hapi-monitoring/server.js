1f5133ed6f1b98d10478828b37194808c37c2faa
Pedro Teixeira <i@pgte.me>
2015-08-06 12:55:19 +0100

    removed good

--- 03-hapi-monitoring/server.js
+++ 03-hapi-monitoring/server.js
@@ -1,6 +1,5 @@
 var Hapi = require('hapi');
 var joi = require('joi');
-var Good = require('good');
 
 var server = module.exports = new Hapi.Server();
 
@@ -11,28 +10,4 @@ server.connection({
   port: process.env.SERVER_PORT || 4000
 });
 
-var events =  {
-  error: '*',
-  wreck: '*',
-  response: '*',
-  log: '*',
-  request: '*'
-};
-
-if (process.env.NODE_ENV === 'test')
-  events = {};
-
-var goodOpts = {
-  reporters: [{
-    reporter: require('good-console'),
-    events: events
-  }]
-};
-
-server.register([{register: Good, options: goodOpts }] , function(err) {
-  if (err)
-    throw err;
-
-  require('./routes');
-});
-
+require('./routes');
